5c823c5993114fb937a2c80c60aee3220b67b6d5
Joscha Feth <joscha@feth.com>
2014-11-20 01:28:53 +0100

    remove Cake as build system

--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cake watch &
-BASE=`pwd`
-cd "$BASE/extension/chrome/js/lib/gmailr/chrome"
-cake watch &
-cd "$BASE/extension/chrome/js"
-cake watch &
-cd "$BASE/extension/chrome/js/lib/gmailui"
-cake watch &
