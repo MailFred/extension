01daea33dc5bababd9bb2e693f12c743393f8863
Joscha Feth <joscha@feth.com>
2014-12-22 05:28:45 +0100

    allow non-minified files

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -4,33 +4,35 @@
 
     var pageMod = require('sdk/page-mod');
     var self = require('sdk/self');
+    var debug = false;
+    var debugInfix = debug ? '' : '.min';
 
     pageMod.PageMod({
         include: [
             '*.mail.google.com'
         ],
         contentStyleFile: [
-            self.data.url('shared/css/styles.css'),
-            self.data.url('shared/bower_components/pikaday/css/pikaday.css')
+            './shared/css/styles.css',
+            './shared/bower_components/pikaday/css/pikaday.css'
         ],
         contentScriptFile: [
+            self.data.url('shared/js/facade' + debugInfix + '.js'),
             self.data.url('shared/bower_components/q/q.js'),
-            self.data.url('shared/js/facade.min.js'),
-            self.data.url('shared/js/trackjs.min.js'),
+            self.data.url('shared/js/trackjs' + debugInfix + '.js'),
             self.data.url('shared/bower_components/trackjs/tracker.js'),
-            self.data.url('shared/bower_components/lodash/dist/lodash.min.js'),
-            self.data.url('shared/bower_components/jquery/dist/jquery.min.js'),
+            self.data.url('shared/bower_components/lodash/dist/lodash' + debugInfix + '.js'),
+            self.data.url('shared/bower_components/jquery/dist/jquery' + debugInfix + '.js'),
 
             self.data.url('shared/bower_components/moment/min/moment.min.js'),
             self.data.url('shared/bower_components/pikaday/pikaday.js'),
 
-            self.data.url('shared/bower_components/yepnope/yepnope.1.5.4-min.js'),
+            self.data.url('shared/bower_components/headjs/dist/1.0.0/head.load' + debugInfix + '.js'),
 
-            self.data.url('shared/js/bootstrap.min.js'),
+            self.data.url('shared/js/bootstrap' + debugInfix + '.js'),
 
-            self.data.url('shared/bower_components/eventr/build/eventr.min.js'),
-            self.data.url('shared/bower_components/gmailui/build/gmailui.min.js'),
-            self.data.url('shared/js/content.min.js')
+            self.data.url('shared/bower_components/eventr/build/eventr' + debugInfix + '.js'),
+            self.data.url('shared/bower_components/gmailui/build/gmailui' + debugInfix + '.js'),
+            self.data.url('shared/js/content' + debugInfix + '.js')
         ],
         contentScriptWhen: 'ready',
         contentScriptOptions: {
