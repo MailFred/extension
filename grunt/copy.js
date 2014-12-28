6165edd4d106b8db1c256eeb3fddead009bf9ab5
Joscha Feth <joscha@feth.com>
2014-12-28 19:22:17 +0100

    use debug flag if needed

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,85 +1,92 @@
-module.exports = function (grunt, options) {
-  'use strict';
+module.exports = function() {
+    'use strict';
+    var debug = false;
+    var infix = debug ? '' : '.min';
 
-  function css(target) {
-    return {
-      "expand": true,
-      "cwd": "shared/css/build",
-      "src": [
-        "**/*.css"
-      ],
-      "dest": target + "/data/shared/css"
-    };
-  }
-
-  function src(target) {
-    return {
-      "expand": true,
-      "cwd": "shared/js/src",
-      "src": [
-        "**/*.coffee"
-      ],
-      "dest": target + "/data/shared/js"
-    };
-  }
-
-  function scripts(target) {
-    return {
-      "expand": true,
-      "cwd": "shared/js/build",
-      "src": [
-        "**/*.{js,map}"
-      ],
-      "dest": target + "/data/shared/js"
-    };
-  }
-
-  function statics(target) {
-    return {
-      "expand": true,
-      "cwd": "shared",
-      "src": [
-          'images/**',
-          'bower_components/eventr/build/eventr.min.js',
-          'bower_components/gmailr/build/gmailr.min.js',
-          'bower_components/gmailui/build/gmailui.min.js',
-          'bower_components/headjs/dist/1.0.0/head.load.min.js',
-          'bower_components/jquery/dist/jquery.min.js',
-          'bower_components/jquery-deparam/jquery.ba-deparam.min.js',
-          'bower_components/lodash/dist/lodash.min.js',
-          'bower_components/lodash/dist/lodash.min.js',
-          'bower_components/moment/min/moment.min.js',
-          'bower_components/pikaday/pikaday.js',
-          'bower_components/q/q.js',
-          'bower_components/trackjs/tracker.js'
-      ],
-      "dest": target + "/data/shared"
-    };
-  }
-
-  return {
-    "options": {
-      "timestamp": true
-    },
-    "shared.styles": {
-      "files": [
-        css('chrome'),
-        css('firefox')
-      ]
-    },
-    "shared.scripts": {
-      "files": [
-        scripts('chrome'),
-        src('chrome'),
-        scripts('firefox'),
-        src('firefox')
-      ]
-    },
-    "shared.static": {
-      "files": [
-        statics('chrome'),
-        statics('firefox')
-      ]
+    function css(target) {
+        return {
+            "expand": true,
+            "cwd": "shared/css/build",
+            "src": [
+                "**/*.css"
+            ],
+            "dest": target + "/data/shared/css"
+        };
     }
-  };
+
+    function src(target) {
+        return {
+            "expand": true,
+            "cwd": "shared/js/src",
+            "src": [
+                "**/*.coffee"
+            ],
+            "dest": target + "/data/shared/js"
+        };
+    }
+
+    function scripts(target) {
+        return {
+            "expand": true,
+            "cwd": "shared/js/build",
+            "src": [
+                debug ? "**/*.{js,map}" : '**/*.min.js'
+            ],
+            "dest": target + "/data/shared/js"
+        };
+    }
+
+    function statics(target) {
+        return {
+            "expand": true,
+            "cwd": "shared",
+            "src": [
+                'images/**',
+                'bower_components/eventr/build/eventr' + infix + '.js',
+                'bower_components/gmailr/build/gmailr' + infix + '.js',
+                'bower_components/gmailui/build/gmailui' + infix + '.js',
+                'bower_components/headjs/dist/1.0.0/head.load' + infix + '.js',
+                'bower_components/jquery/dist/jquery' + infix + '.js',
+                'bower_components/jquery-deparam/jquery.ba-deparam' + infix + '.js',
+                'bower_components/lodash/dist/lodash' + infix + '.js',
+                'bower_components/moment/min/moment' + infix + '.js',
+                'bower_components/pikaday/pikaday.js',
+                'bower_components/q/q.js',
+                'bower_components/trackjs/tracker.js'
+            ],
+            "dest": target + "/data/shared"
+        };
+    }
+
+    var config = {
+        "options": {
+            "timestamp": true
+        },
+        "shared.styles": {
+            "files": [
+                css('chrome'),
+                css('firefox')
+            ]
+        },
+        "shared.scripts": {
+            "files": [
+                scripts('chrome'),
+                scripts('firefox')
+            ]
+        },
+        "shared.static": {
+            "files": [
+                statics('chrome'),
+                statics('firefox')
+            ]
+        }
+    };
+
+    if (debug) {
+        config['shared.scripts'].files.push(src('chrome'));
+        config['shared.scripts'].files.push(src('firefox'));
+    }
+
+
+    return config;
 };
