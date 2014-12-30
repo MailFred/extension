d3d2da23f497b1b642c0c97b569bd74dbb2ae2fb
Joscha Feth <joscha@feth.com>
2014-12-30 17:10:26 +0100

    install xpisign with sudo

--- postinstall.sh
+++ postinstall.sh
@@ -4,4 +4,4 @@ bower install
 # Install tools for signing XPI files
 # If we have OpenSSL, we don't need M2Crypto, although it is recommended
 # easy_install -Z M2Crypto
-pip install --no-dependencies https://github.com/nmaier/xpisign.py/zipball/master
+sudo pip install --no-dependencies https://github.com/nmaier/xpisign.py/zipball/master
