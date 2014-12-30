4a9c8a265104c36e2f96b95b5227236d3a16b0b5
Joscha Feth <joscha@feth.com>
2014-12-30 16:44:44 +0100

    do not install M2Crypto

--- postinstall.sh
+++ postinstall.sh
@@ -2,5 +2,6 @@
 # Install JS packages
 bower install
 # Install tools for signing XPI files
-easy_install -Z M2Crypto
+# If we have OpenSSL, we don't need M2Crypto, although it is recommended
+# easy_install -Z M2Crypto
 pip install https://github.com/nmaier/xpisign.py/zipball/master
