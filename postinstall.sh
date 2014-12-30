d0c1e8a968eaa4b818c5a6d2fa02812a212b29f1
Joscha Feth <joscha@feth.com>
2014-12-30 17:04:11 +0100

    do not install deps for xpisign

--- postinstall.sh
+++ postinstall.sh
@@ -4,5 +4,4 @@ bower install
 # Install tools for signing XPI files
 # If we have OpenSSL, we don't need M2Crypto, although it is recommended
 # easy_install -Z M2Crypto
-env SWIG_FEATURES="-cpperraswarn -includeall -D__`uname -m`__ -I/usr/include/openssl" pip install M2Crypto
-pip install https://github.com/nmaier/xpisign.py/zipball/master
+pip install --no-dependencies https://github.com/nmaier/xpisign.py/zipball/master
