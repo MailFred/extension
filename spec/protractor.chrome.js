56922d172417f5364b38f20295923f2ec67dc751
Joscha Feth <joscha@feth.com>
2015-10-18 16:08:02 +1100

    style: better readable paths

--- spec/protractor.chrome.js
+++ spec/protractor.chrome.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var helper = require('./helper.js');
+var path = require('path');
 
 /**
  * Reads a given file path into a base64 encoded string
@@ -32,11 +33,13 @@ if (helper.isSauceLabsRun()) {
     helper.enhanceConfigWithSauceLabsData(config);
     helper.enhanceCapabilitiesWithSauceLabsData(config.capabilities);
 
+    var pathToCrx = 'build/mailfred-extension-' + helper.readPkg().version + '.crx';
     chromeOptions.extensions = [
-        fileToBase64String('build/mailfred-extension-' + helper.readPkg().version + '.crx')
+        fileToBase64String(pathToCrx)
     ];
 } else {
-    chromeOptions.args.push("--load-extension=chrome");
+    var pathToChromeDir = path.join(__dirname, '..', 'chrome');
+    chromeOptions.args.push('--load-extension=' + pathToChromeDir);
 }
 
 exports.config = config;
